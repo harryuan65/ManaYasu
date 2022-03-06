af39febc2e90275df183e70e4a454e61612d4a76
harryuan65 <harryuan.65@gmail.com>
2022-03-06 12:13:35 +0800

    chore: remove unused packages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgrPlugin from '@honkhonk/vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    svgrPlugin({
-      svgrOptions: {
-        icon: true,
-      },
-    }),
-  ],
+  plugins: [react()],
 });
