34bf9314aa84a83b645511768f4a718953dc63ed
harryuan65 <harryuan.65@gmail.com>
2022-03-13 00:10:26 +0800

    chore: disable space-before-function-paren

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,22 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: ['plugin:react/recommended', 'standard'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'no-use-before-define': 'off',
     'react/display-name': [0, { ignoreTranspilerName: 0 }],
     semi: 'off',
-    'comma-dangle': 'off'
-  }
-}
+    'comma-dangle': 'off',
+    'space-before-function-paren': 'off',
+  },
+};
